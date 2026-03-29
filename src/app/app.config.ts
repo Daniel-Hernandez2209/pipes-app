c25f29d3f9933410a2257c18892dbd29fc6a651d
Daniel Hernandez <danielakire07@gmail.com>
2026-03-29 01:16:20 -0500

    build(deps): install dependencies and setup vscode configuration
    - Add required Angular packages and libraries
    - Configure VSCode settings for development
    - Update app configuration for project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import localeAleman from '@angular/common/locales/de-AT';
+import localeEs from '@angular/common/locales/es-PR';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeAleman, 'de-AT');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'de-AT',
+    },
+  ],
 };
