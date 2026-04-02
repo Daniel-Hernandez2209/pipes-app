9fc12fe1f864c359ad332676fa2cecbdb48cf539
Daniel Hernandez <danielakire07@gmail.com>
2026-04-02 17:47:55 -0500

    se usa el pipe de i18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
+import { I18nSelectPipe } from '@angular/common';
+
+const cliente1 = {
+  name: 'Daniel',
+  gender: 'male',
+  'edad ': 33,
+};
+
+const cliente2 = {
+  name: 'Maria',
+  gender: 'female',
+  'edad ': 28,
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+  client = signal(cliente1);
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeCliente() {
+    if (this.client() === cliente1) {
+      this.client.set(cliente2);
+
+      return;
+    }
+    this.client.set(cliente1);
+  }
+}
